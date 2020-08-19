03abe149d95152e0fd23739748016eb1a40de937
Arye Eidelman <aryedov.e@gmail.com>
2020-08-19 01:13:28 -0400

    Reorganize api/game/index.php by routes

--- api/game/index.php
+++ api/game/index.php
@@ -1,10 +1,16 @@
 <?php
-include '../../main.php';
-connect_to_database();
-if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST') {
+// route: create game
+if (strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
+  include '../../main.php';
+  connect_to_database();
   $game = create_game($_POST["player_name"], $_POST["color"]);
+  disconnect_from_database();
+  $data = ["game" => $game];
+  header('Content-Type: application/json');
+  echo json_encode($data);
+
+// route: unknown
+} else {
+  http_response_code(404);
+  echo json_encode(["error" => "404 at " . $_SERVER['REQUEST_METHOD'] . " " . $_SERVER['REQUEST_URI']]);
 }
-disconnect_from_database();
-$data = ["game" => $game];
-header('Content-Type: application/json');
-echo json_encode($data);
